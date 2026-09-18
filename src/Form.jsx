79bda0e0c856a2a1b1624b279f9bba5b7116c58f
Rupa Kumari <rupakumari18105@gmail.com>
2026-09-18 22:54:49 +0530

    Update Chef Claude

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,14 +1,26 @@
 export default function Form() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+  const ingredientsItems = ingredients.map((ing) => <li key={ing}>{ing}</li>);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIng = formData.get("ingredient");
+    ingredients.push(newIng);
+    console.log(ingredients);
+  }
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add ingredients"
+          name="ingredient"
         />
         <button>Add ingredients</button>
       </form>
+      <ul>{ingredientsItems}</ul>
     </main>
   );
 }
